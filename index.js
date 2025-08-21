6d2ac4518294b13032e52059216fe1f383a74777
xcaio <caiosantos1703@gmail.com>
2025-08-21 11:00:42 -0300

    understanding routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res)=>{
-    res.send("Olá Alterado")
+    const soma = 1+2
+    res.send({soma: soma})
 })
-app.listen(3000, '', console.log("Servidor inicializado"))
+app.listen(3000, '', console.log("Servidor inicializado"))  
